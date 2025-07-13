45e8a6ac246545bf0a57b1c5acf571aa8cd275fa
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-07-13 15:25:16 +0530

    working with server side folder structure and  made controller and routes file

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import authRoutes from './routes/auth.route.js'
+
+
+const app = express()
+const PORT = process.env.PORT || 1111;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! from backend')
+})
+
+// auth routes
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
