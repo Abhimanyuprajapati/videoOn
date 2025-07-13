cc2740419e3a505a52e6b08608b7bd2370975ac5
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-07-13 17:52:29 +0530

    Implement user signup functionality with validation and password hashing; add User model and database connection

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,70 @@
-export async function signup(req, res){
-    res.send("Signup Route");
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  // res.send("Signup Route");
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All Fields are required" });
+    }
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters long" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exits, Please try different email" });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1; // generate 1 to 100 random number
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = new User.create({
+      fullName,
+      email,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "15d",
+      }
+    );
+
+    // create the user in the stream
+
+    
+    res.cookie("jwt", token, {
+      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+      sameSite: "Strict", // Helps prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-export async function login(req, res){
-    res.send("login Route");
+export async function login(req, res) {
+  res.send("login Route");
 }
 
-export function logout(req, res){
-    res.send("logout Route");
+export function logout(req, res) {
+  res.send("logout Route");
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
 
 
 const app = express()
 const PORT = process.env.PORT || 1111;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World! from backend')
@@ -16,4 +19,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
+  connectDB();
 })
