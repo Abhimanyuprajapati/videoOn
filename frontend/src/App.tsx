824fc7706f0530fee187945b35a5cbf51a34646d
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-08-04 00:59:37 +0530

    feat: add routing with react-router-dom and create basic page components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,10 @@
 function App() {
 
   return (
-   <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-   </>
+   <div className="h-screen" data-theme="night">
+
+
+   </div>
   )
 }
 
