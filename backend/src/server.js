7ef1a90ebdd775ce24ac940c86bd2f9b15b92b82
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-08-08 00:40:11 +0530

    feat: enable CORS for API requests from the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import cors from 'cors'
 import authRoutes from './routes/auth.route.js'
 import userRoutes from './routes/user.route.js'
 import chatRoutes from './routes/chat.route.js'
@@ -8,9 +9,14 @@ import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser'
 
 
+
 const app = express()
 const PORT = process.env.PORT || 1111;
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
